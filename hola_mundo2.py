7f8bb246e44ae5aa64001f56d4e461fbb9e721cc
cislast <67597486+cislast@users.noreply.github.com>
2020-07-03 16:20:29 -0500

    Subi un ejemplo de diccionario

---
+++ hola_mundo2.py
@@ -0,0 +1,19 @@
+#print ("Hola Mundo de chocolate")
+#entero=25
+#texto = "Un texto XX"
+#print ("Hola desde Python")
+#print ('Tambien las comillas simples funcionan')
+#valor = input("Ingresa algo ")
+#print ("Ingresaste ", valor)
+#n1=float(input ("Dame un numero: ")) 
+#n2=float(input ("Dame otro número: "))
+#suma=n1+n2
+#print ("La suma es: ", suma)
+#print ("El entero es: ", entero )
+#finalizar= "No"
+#while (finalizar not in ['Si', 'SI', 'si']):
+ #   finalizar = input('Finalizar programa ? ')
+arreglo = [1,2,3,4,5,6,7]
+x=input("Dame un valor X ")
+resultado=arreglo[int (x)]*2
+print("Resultado: ", resultado)
